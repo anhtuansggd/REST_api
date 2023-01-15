240f716d76cf60a8a72bc898f75f24b3c2ff7dd3
anhtuansggd <anhtuan.sggd@gmail.com>
2023-01-16 00:47:52 +0100

    REST_api

---
+++ server.js
@@ -0,0 +1,19 @@
+//sudo service mongod restart
+//npm run devStart
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.DATABASE_URL, {useNewURLParser: true})
+const db = mongoose.connection
+db.on('error', (error)=>console.error(error))
+db.once('open', ()=>console.log('Connected to Databases'))
+
+app.use(express.json())
+
+const subscribersRouter = require('/home/tuan/Study/TMP2/REST_api/routes/subscribers')
+app.use('/subscribers', subscribersRouter)
+
+
+app.listen(3000, () => console.log('Server Started'))
